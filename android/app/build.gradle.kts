6792f8332a2533d8b8c3e5ca49a16cebf8e45e6d
Utk4rxh <159775654+Utk4rxh@users.noreply.github.com>
2026-07-31 22:20:20 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.lokbus_clean"
+    compileSdk = 36   // ✅ Force latest SDK
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.lokbus_clean"
+        minSdk = flutter.minSdkVersion  // ✅ ya jo bhi tumhe chahiye (Geolocator ke liye 21+ required)
+        targetSdk = 36  // ✅ Force latest targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/lokbus_clean/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lokbus_clean
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
