834e5bc237d2f24ef05c160536cee26975a7ca56
Ashish Sharma <stackdev.ash@gmail.com>
2026-08-03 18:59:40 +0530

    feat: add Haversine distance calculation utility

---
+++ backend/utils/geo.js
@@ -0,0 +1,18 @@
+function toRad(deg) { 
+  return deg * Math.PI / 180; 
+}
+
+function haversineMeters([lng1, lat1], [lng2, lat2]) {
+  const R = 6371000;
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lng2 - lng1);
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
+
+module.exports = {
+  haversineMeters
+};
